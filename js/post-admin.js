fbb17aec1a5cbb1c23f10a19e3b4b05246204702
tlovett1 <tlovett1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-20 21:42:03 +0000

    Version 0.3.0
    git-svn-id: https://plugins.svn.wordpress.org/editorial-access-manager/tags/0.3.0@1029682 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/post-admin.js
@@ -0,0 +1,36 @@
+( function( $, undefined ) {
+
+	'use strict';
+
+	var $rolesSelect = $( document.getElementById( 'eam_allowed_roles' ) );
+	var $usersSelect = $( document.getElementById( 'eam_allowed_users' ) );
+	var accessControlSelect = document.getElementById( 'eam_enable_custom_access' );
+	var roleControls = document.getElementById( 'eam_control_roles' );
+	var usersControls = document.getElementById( 'eam_control_users' );
+
+	$rolesSelect.chosen( {
+		width: 'auto'
+	});
+
+	$usersSelect.chosen( {
+		width: 'auto'
+	});
+
+	function conditionallyRevealControls() {
+		if ( accessControlSelect.value === 'users' ) {
+			usersControls.style.display = 'block';
+			roleControls.style.display = 'none';
+		} else if ( accessControlSelect.value === 'roles' ) {
+			usersControls.style.display = 'none';
+			roleControls.style.display = 'block';
+		} else {
+			usersControls.style.display = 'none';
+			roleControls.style.display = 'none';
+		}
+	}
+
+	conditionallyRevealControls();
+
+	accessControlSelect.onchange = conditionallyRevealControls;
+
+})( jQuery );
